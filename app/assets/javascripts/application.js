f0564427f3470b307250cf6333606bfc39ec0d47
Kenzie Ryan <kenzkenz314@gmail.com>
2019-07-19 22:24:59 +0000

    font4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require popper
-//= require tether
-//= require_tree .
-//= require jquery-ui
+//= require bootstrap
